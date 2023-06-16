0e224e5bee635e52e2f90d70d2bec47a6ad35a84
O Teerasak Vichadee <iolumin@gmail.com>
2023-06-16 11:18:53 +0700

    update storybook config
    - allow storybook to read all *.stories.* in lib/

--- libs/storybook-host/.storybook/main.ts
+++ libs/storybook-host/.storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: [
+    '../../**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/react-vite',
